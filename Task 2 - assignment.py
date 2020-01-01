ea8ad229d06013d869a8de482835473977d61c06
learninfo <59413414+learninfo@users.noreply.github.com>
2020-01-01 18:20:00 +0000

    Python - task 2 Assignment

---
+++ Task 2 - assignment.py
@@ -0,0 +1,10 @@
+import turtle
+sides = 3
+size = 100
+angle = 360/sides
+turtle.forward(size)
+turtle.right(angle)
+turtle.forward(size)
+turtle.right(angle)
+turtle.forward(size)
+turtle.right(angle)
